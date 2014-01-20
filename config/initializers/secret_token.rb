6cdacae71dfcadae2af0eb12fc06c65c73d72138
Elizabeth Rapoport <elizrap@mit.edu>
2014-01-20 12:11:57 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapDemo::Application.config.secret_token = 'd6ddbd1973b21da4a40fde9364f2f22674f899094198f2c08a6b326118bc52497b71a25dab9dd1640b77b8f1a158f3221d002c5be87aed69922f703342b7bba2'
