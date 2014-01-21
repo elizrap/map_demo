a7db664b8aab1c5457093451074198f1c3af0e38
Elizabeth Rapoport <elizrap@mit.edu>
2014-01-20 19:54:12 -0500

    show infowindow on click of city marker, but also hide city markers

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,23 @@
 class CitiesController < ApplicationController
+
   def index
     @cities = City.all
-    @json = @cities.to_gmaps4rails
+    @json = @cities.to_gmaps4rails do |city, marker|
+      marker.infowindow render_to_string(:partial => "/cities/infowindow", :locals => { :cat => city})
+      marker.title "#{city.name}"
+      marker.json({ :population => city.population})
+      marker.picture({
+                            :picture => "http://forum.placeboworld.co.uk/forum/images/smilies/Black_Cat_by_angelwolf2002.gif",
+                            :width => 32,
+                            :height => 32})
+
+    end
   end
+
+
+
+
 end
+
+
+
